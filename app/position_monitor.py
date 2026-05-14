d927d81e9da4d143a7e9cb1dba14d0925621297b
garybull <garybull781@gmail.com>
2026-05-13 20:57:23 -0500

    fix

--- app/position_monitor.py
+++ app/position_monitor.py
@@ -11,7 +11,7 @@ from app.logger import logger
 from app.emailer import send_email
 
 
-DB_PATH = "DB_PATH = "/home/ubuntu/SwingTrading/trading_system.db"
+DB_PATH = "/home/ubuntu/SwingTrading/trading_system.db"
 
 
 # =====================================
